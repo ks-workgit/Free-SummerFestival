245f6a6eb1c02344ac9c0cb490badadd9476c338
s_work <hmj2422017@stu.o-hara.ac.jp>
2025-07-25 09:04:09 +0900

    なんか残ってたわ

--- Assets/Anomaly/Scripts/GameClear.cs
+++ Assets/Anomaly/Scripts/GameClear.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GameClear : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AnomalyManager m_anomalyManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Update()
+	{
+		m_anomalyManager.GetCurrentNum();
+	}
 }
